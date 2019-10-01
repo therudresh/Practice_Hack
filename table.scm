2dbaeb7f63d15b562162df02c4e32b860be1ab19
therudresh <sheelbmw@gmail.com>
2019-10-01 10:42:25 -0400

    Update table.scm

--- table.scm
+++ table.scm
@@ -3,17 +3,17 @@
     (if record
       (cdr record)
       #f)))
-;; (define (assoc key records)
-;;   (cond ((null? records) #f)
-;;    ((equal? key (caar records)) (car records))
-;;    (else (assoc key (cdr records)))))
+(define (assoc key records)
+   (cond ((null? records) #f)
+      ((equal? key (caar records)) (car records))
+      (else (assoc key (cdr records)))))
 
 (define (insert! key value table)
-(let ((record (assoc key (cdr table))))
-(if record
-(set-cdr! record value)
-(set-cdr! table
-(cons (cons key value) (cdr table)))))
-’ok) ;;; maybe val would be better than ’ok
+  (let ((record (assoc key (cdr table))))
+    (if record
+      (set-cdr! record value)
+      (set-cdr! table
+
+(cons (cons key value) (cdr table))))) ’ok) ;;; maybe val would be better than ’ok
 (define (make-table)
 (list ’*table*))
